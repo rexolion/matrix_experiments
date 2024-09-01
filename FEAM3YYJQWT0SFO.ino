b3335910cf5b25a9ddbf15422108bc9fd07f46ac
Artyom Alekseev <contact@artyomalekseev.com>
2024-09-01 07:42:15 -0700

    init

---
+++ FEAM3YYJQWT0SFO.ino
@@ -0,0 +1,232 @@
+#include "LedControl.h"
+#include <DHT.h>
+
+#define DHTPIN 2  // Digital pin connected to the DHT sensor
+// Feather HUZZAH ESP8266 note: use pins 3, 4, 5, 12, 13 or 14 --
+// Pin 15 can work but DHT must be disconnected during program upload.
+
+// Uncomment whatever type you're using!
+//#define DHTTYPE DHT11   // DHT 11
+#define DHTTYPE DHT22  // DHT 22  (AM2302), AM2321
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+LedControl lc = LedControl(11, 13, 10, 4);  // Pins: DIN,CLK,CS, # of Display connected
+
+unsigned long delayTime = 200;  // Delay between Frames
+
+// Put values in arrays
+byte invader1a[] = {
+  B00011000,  // First frame of invader #1
+  B00111100,
+  B01111110,
+  B11011011,
+  B11111111,
+  B00100100,
+  B01011010,
+  B10100101
+};
+
+byte invader1b[] = {
+  B00011000,  // Second frame of invader #1
+  B00111100,
+  B01111110,
+  B11011011,
+  B11111111,
+  B00100100,
+  B01011010,
+  B01000010
+};
+
+byte invader2a[] = {
+  B00100100,  // First frame of invader #2
+  B00100100,
+  B01111110,
+  B11011011,
+  B11111111,
+  B11111111,
+  B10100101,
+  B00100100
+};
+
+byte invader2b[] = {
+  B00100100,  // Second frame of invader #2
+  B10100101,
+  B11111111,
+  B11011011,
+  B11111111,
+  B01111110,
+  B00100100,
+  B01000010
+};
+
+byte invader3a[] = {
+  B00011000,  // First frame of invader #1
+  B00111100,
+  B01111110,
+  B11011011,
+  B11111111,
+  B00100100,
+  B01011010,
+  B10100101
+};
+
+byte invader3b[] = {
+  B00011000,  // Second frame of invader #1
+  B00111100,
+  B01111110,
+  B11011011,
+  B11111111,
+  B00100100,
+  B01011010,
+  B01000010
+};
+
+byte invader4a[] = {
+  B00100100,  // First frame of invader #2
+  B00100100,
+  B01111110,
+  B11011011,
+  B11111111,
+  B11111111,
+  B10100101,
+  B00100100
+};
+
+byte invader4b[] = {
+  B00100100,  // Second frame of invader #2
+  B10100101,
+  B11111111,
+  B11011011,
+  B11111111,
+  B01111110,
+  B00100100,
+  B01000010
+};
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);     // Start serial communication
+  lc.shutdown(0, false);  // Wake up displays
+  lc.shutdown(1, false);
+  lc.shutdown(2, false);
+  lc.shutdown(3, false);
+  lc.setIntensity(0, 5);  // Set intensity levels
+  lc.setIntensity(1, 5);
+  lc.setIntensity(2, 5);
+  lc.setIntensity(3, 5);
+  lc.clearDisplay(0);  // Clear Displays
+  lc.clearDisplay(1);
+  lc.clearDisplay(2);
+  lc.clearDisplay(3);
+}
+
+
+//  Take values in Arrays and Display them
+void sinvader1a() {
+  for (int i = 0; i < 8; i++) {
+    lc.setRow(0, i, invader1a[i]);
+  }
+}
+
+void sinvader1b() {
+  for (int i = 0; i < 8; i++) {
+    lc.setRow(0, i, invader1b[i]);
+  }
+}
+
+void sinvader2a() {
+  for (int i = 0; i < 8; i++) {
+    lc.setRow(1, i, invader2a[i]);
+  }
+}
+
+void sinvader2b() {
+  for (int i = 0; i < 8; i++) {
+    lc.setRow(1, i, invader2b[i]);
+  }
+}
+
+void sinvader3a() {
+  for (int i = 0; i < 8; i++) {
+    lc.setRow(2, i, invader1a[i]);
+  }
+}
+
+void sinvader3b() {
+  for (int i = 0; i < 8; i++) {
+    lc.setRow(2, i, invader1b[i]);
+  }
+}
+
+void sinvader4a() {
+  for (int i = 0; i < 8; i++) {
+    lc.setRow(3, i, invader2a[i]);
+  }
+}
+
+void sinvader4b() {
+  for (int i = 0; i < 8; i++) {
+    lc.setRow(3, i, invader2b[i]);
+  }
+}
+
+void loop() {
+  // Put #1 frame on both Display
+  sinvader1a();
+  delay(delayTime);
+  sinvader2a();
+  delay(delayTime);
+
+
+  // Put #2 frame on both Display
+  sinvader1b();
+  delay(delayTime);
+  sinvader2b();
+  delay(delayTime);
+
+  // Put #3 frame on both Display
+  sinvader3a();
+  delay(delayTime);
+  sinvader4a();
+  delay(delayTime);
+
+
+  // Put #4 frame on both Display
+  sinvader3b();
+  delay(delayTime);
+  sinvader4b();
+  delay(delayTime);
+  // Wait a few seconds between measurements.
+  delay(2000);
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+  // Compute heat index in Fahrenheit (the default)
+  float hif = dht.computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.print(F("°C "));
+  Serial.print(f);
+  Serial.print(F("°F  Heat index: "));
+  Serial.print(hic);
+  Serial.print(F("°C "));
+  Serial.print(hif);
+  Serial.println(F("°F"));
+}
