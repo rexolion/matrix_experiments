571f8c13252c7088d8a95ccde457caddbe156490
Artyom Alekseev <contact@artyomalekseev.com>
2024-09-16 15:41:31 +0200

    remove DHT

--- FEAM3YYJQWT0SFO.ino
+++ FEAM3YYJQWT0SFO.ino
@@ -1,16 +1,7 @@
 #include "LedControl.h"
-#include <DHT.h>
 
 #define MIC_PIN A1 // Microphone is attached to this analog pin
-#define DHTPIN 2   // Digital pin connected to the DHT sensor
-// Feather HUZZAH ESP8266 note: use pins 3, 4, 5, 12, 13 or 14 --
-// Pin 15 can work but DHT must be disconnected during program upload.
 
-// Uncomment whatever type you're using!
-// #define DHTTYPE DHT11   // DHT 11
-#define DHTTYPE DHT22 // DHT 22  (AM2302), AM2321
-
-// #define DHTTYPE DHT21   // DHT 21 (AM2301)
 LedControl lc = LedControl(11, 13, 10, 4); // Pins: DIN,CLK,CS, # of Display connected
 #define INPUT_FLOOR 10                     // Lower range of analogRead input
 #define INPUT_CEILING 300                  // Max range of analogRead input, the lower the value the more sensitive (1023 = max)
@@ -108,8 +99,6 @@ byte invader4b[] = {
     B00100100,
     B00011000,
 };
-
-DHT dht(DHTPIN, DHTTYPE);
 
 void setup()
 {
@@ -246,38 +235,4 @@ void loop()
   delay(delayTime);
   sinvader4b();
   delay(delayTime);
-  // Wait a few seconds between measurements.
-  delay(2000);
-
-  // Reading temperature or humidity takes about 250 milliseconds!
-  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-  float h = dht.readHumidity();
-  // Read temperature as Celsius (the default)
-  float t = dht.readTemperature();
-  // Read temperature as Fahrenheit (isFahrenheit = true)
-  float f = dht.readTemperature(true);
-
-  // Check if any reads failed and exit early (to try again).
-  if (isnan(h) || isnan(t) || isnan(f))
-  {
-    Serial.println(F("Failed to read from DHT sensor!"));
-    return;
-  }
-
-  // Compute heat index in Fahrenheit (the default)
-  float hif = dht.computeHeatIndex(f, h);
-  // Compute heat index in Celsius (isFahreheit = false)
-  float hic = dht.computeHeatIndex(t, h, false);
-
-  Serial.print(F("Humidity: "));
-  Serial.print(h);
-  Serial.print(F("%  Temperature: "));
-  Serial.print(t);
-  Serial.print(F("°C "));
-  Serial.print(f);
-  Serial.print(F("°F  Heat index: "));
-  Serial.print(hic);
-  Serial.print(F("°C "));
-  Serial.print(hif);
-  Serial.println(F("°F"));
 }
